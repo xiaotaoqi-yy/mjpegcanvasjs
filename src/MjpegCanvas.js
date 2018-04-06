76eed36443eeb7cd24f4abbd24aadcd6ee3aa537
Yu Yan <yyan0625@outlook.com>
2018-04-06 18:18:25 +0800

    Update MjpegCanvas.js

--- src/MjpegCanvas.js
+++ src/MjpegCanvas.js
@@ -1,7 +1,7 @@
 /**
  * @author Russell Toris - russell.toris@gmail.com
  */
-
+var EventEmitter2 = require('eventemitter2').EventEmitter2;
 var MJPEGCANVAS = MJPEGCANVAS || {
   REVISION : '0.5.0-SNAPSHOT'
 };
